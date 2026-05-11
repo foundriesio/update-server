12f50f5cfd89b5680acd86af85887a1e3de844f2
Andy Doan <andy@foundries.io>
2026-05-11 16:27:36 -0500

    cmd: Allow user-add to create token
    We need the ability to create a token for e2e integration tests
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cmd/server/user_add.go
+++ cmd/server/user_add.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"golang.org/x/term"
 
@@ -19,6 +20,7 @@ type UserAddCmd struct {
 	Username      string   `arg:"required" help:"Username for the new user"`
 	Password      string   `arg:"" help:"Password for the new user (read from stdin if not provided)"`
 	AllowedScopes []string `arg:"" help:"Roles to assign to the new user"`
+	TokenFile     string   `arg:"" help:"File to write an API token for the new user (optional)"`
 }
 
 func (c UserAddCmd) Run(args CommonArgs) error {
@@ -81,5 +83,22 @@ func (c UserAddCmd) Run(args CommonArgs) error {
 		AllowedScopes: scopes,
 	}
 
-	return userStorage.Create(u)
+	if err := userStorage.Create(u); err != nil {
+		return fmt.Errorf("failed to create user: %w", err)
+	}
+	u, err = userStorage.Get(c.Username)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve created user: %w", err)
+	}
+	if c.TokenFile != "" {
+		expiresAt := time.Now().Add(30 * 24 * time.Hour)
+		token, err := u.GenerateToken("user-add-cli", expiresAt.Unix(), scopes)
+		if err != nil {
+			return fmt.Errorf("failed to generate API token: %w", err)
+		}
+		if err := os.WriteFile(c.TokenFile, []byte(token.Value), 0600); err != nil {
+			return fmt.Errorf("failed to write API token to file: %w", err)
+		}
+	}
+	return nil
 }
