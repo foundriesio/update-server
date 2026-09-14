62ddf1af6a4e9ec2762f7e1cc5333befba59f347
Andy Doan <doanac@qti.qualcomm.com>
2026-09-14 13:59:12 -0500

    storage: Use single quotes for DB empty strings
    Using double quotes causes problem if you want to perform DB table
    alterations. For example, I wanted to change the `pubkey` column to
    `cert` and sqlite complains with:
    ```
    sqlite> ALTER TABLE devices RENAME COLUMN pubkey TO cert;
    Error near line 2: error in table devices: no such column: "" - should this be a string literal in single-quotes?
    ```
    
    Once all empty strings were changed to single quotes, the command will
    work. We should update this before we ship a v1 to make potential DB
    migrations easier in the future.
    
    Signed-off-by: Andy Doan <doanac@qti.qualcomm.com>

--- storage/db.go
+++ storage/db.go
@@ -83,24 +83,24 @@ func createTables(db *sql.DB) error {
 			deleted BOOL DEFAULT 0,
 			created_at INT DEFAULT 0,
 			last_seen INT DEFAULT 0,
-			tag VARCHAR(80) DEFAULT "",
+			tag VARCHAR(80) DEFAULT '',
 			labels JSONB(2048) DEFAULT "{}",
-			update_name VARCHAR(80) DEFAULT "",
-			target_name VARCHAR(80) DEFAULT "",
-			ostree_hash VARCHAR(80) DEFAULT "",
-			apps VARCHAR(2048) DEFAULT "",
+			update_name VARCHAR(80) DEFAULT '',
+			target_name VARCHAR(80) DEFAULT '',
+			ostree_hash VARCHAR(80) DEFAULT '',
+			apps VARCHAR(2048) DEFAULT '',
 
 			group_name_modified_at INT DEFAULT 0,
 
 			name VARCHAR(80) GENERATED ALWAYS AS (
-				COALESCE(labels ->> '$.name', "")
+				COALESCE(labels ->> '$.name', '')
 			) VIRTUAL,
 			group_name VARCHAR(80) GENERATED ALWAYS AS (
-				COALESCE(labels ->> '$.group', "")
+				COALESCE(labels ->> '$.group', '')
 			) VIRTUAL
 		) WITHOUT ROWID;
 
-		CREATE UNIQUE INDEX idx_device_name_unique ON devices(name) WHERE name != "";
+		CREATE UNIQUE INDEX idx_device_name_unique ON devices(name) WHERE name != '';
 		CREATE INDEX idx_device_name ON devices(name);
 		CREATE INDEX idx_device_group ON devices(group_name);
 
@@ -132,7 +132,7 @@ func createTables(db *sql.DB) error {
 			email          TEXT,
 			created_at     INT DEFAULT 0,
 			deleted        BOOL DEFAULT 0,
-			allowed_scopes TEXT DEFAULT "",
+			allowed_scopes TEXT DEFAULT '',
 
 			auth_provider_data JSONB NOT NULL DEFAULT '{}'
 		);
@@ -163,7 +163,7 @@ func createTables(db *sql.DB) error {
 			tag         VARCHAR(80) NOT NULL,
 			name        VARCHAR(80) NOT NULL,
 			uploaded_at INT NOT NULL DEFAULT 0,
-			uploaded_by TEXT NOT NULL DEFAULT "",
+			uploaded_by TEXT NOT NULL DEFAULT '',
 			PRIMARY KEY (tag, name)
 		) WITHOUT ROWID;
 
