28df9954e649a81f04b5b3cbac3708e68aa4ff5c
Andy Doan <andy@foundries.io>
2026-09-16 11:47:18 -0500

    swagger: Remove old "Satellite" references
    Signed-off-by: Andy Doan <andy@foundries.io>

--- server/gateway/doc.go
+++ server/gateway/doc.go
@@ -1,7 +1,7 @@
 // Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
-// @title DG Satellite Gateway API
+// @title Update Server Gateway API
 // @version 1
 // @description Device gateway API for mTLS-authenticated device communication.
 // @BasePath /
